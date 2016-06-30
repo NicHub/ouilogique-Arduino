2f4eb8d8d6a12181d9b51997d01bd12c485149e9
NicHub <nicolas.jeanmonod@gmail.com>
2016-06-30 22:10:09 +0200

    encodeur-rotatif-KY-040.ino

---
+++ encodeur-rotatif-KY-040/encodeur-rotatif-KY-040.ino
@@ -0,0 +1,129 @@
+/*
+
+LECTURE D’UN ENCODEUR ROTATIF KY-040
+====================================
+
+# DESCRIPTION DU PROGRAMME
+Lecture d’un encodeur rotatif KY-040 pour affichage dans le traceur
+série de l’IDE Arduino (⌘⇧L).
+La rotation du bouton est détecté avec des interruptions sur les
+broches 2 et 3 du port D. Comme l’Arduino Nano n’a que ces deux broches
+avec interruption, le bouton de l’encodeur (quand on presse dessus) est
+lu par polling.
+Ce programme implémente un filtrage des rebond en deux étapes (voir
+la procédure “lectureEncodeur”).
+
+Ce programme est basé sur celui d’Oleg Mazurov
+https://www.circuitsathome.com/mcu/reading-rotary-encoder-on-arduino
+
+# RÉFÉRENCE DE L’ENCODEUR
+http://www.banggood.com/5Pcs-5V-KY-040-Rotary-Encoder-Module-For-Arduino-AVR-PIC-p-951151.html
+
+# CONNEXIONS DE L’ENCODEUR KY-040 SUR ARDUINO NANO
+    GND            ⇒   GND
+    +              ⇒   +5V
+    SW  (bouton)   ⇒   pin D4 (PORT D4)
+    DT  (encodeur) ⇒   pin D3 (PORT D3)
+    CLK (encodeur) ⇒   pin D2 (PORT D2)
+
+juin 2016, ouilogique.com
+
+*/
+
+#define ENC_A 2
+#define ENC_B 3
+#define ENC_C 4
+#define ENC_PORT PIND
+#define BtnRead  ! bitRead( ENC_PORT, ENC_C )
+
+bool encodeurTourne = false;
+byte ENC_PORT_VAL = 0;
+
+
+void setup()
+{
+  pinMode( ENC_A, INPUT_PULLUP );
+  pinMode( ENC_B, INPUT_PULLUP );
+  pinMode( ENC_C, INPUT_PULLUP );
+
+  attachInterrupt( 0, interruptionEncodeur, CHANGE );
+  attachInterrupt( 1, interruptionEncodeur, CHANGE );
+  encodeurTourne = true;
+
+  Serial.begin( 115200 );
+}
+
+
+void loop()
+{
+  static uint8_t counter = 0;
+
+  // Si l’encodeur est tourné.
+  if( encodeurTourne )
+  {
+    int8_t encodeurVal = lectureEncodeur();
+    if( encodeurVal != 0 )
+    {
+      counter += encodeurVal;
+      // À visionner dans le traceur série de l’IDE Arduino (⌘⇧L)
+      Serial.println( counter, DEC );
+    }
+    encodeurTourne = false;
+  }
+
+  // Si le bouton est pressé, on remet le compteur à 0.
+  if( BtnRead )
+    { counter = 0; }
+}
+
+
+int8_t lectureEncodeur()
+{
+  // Lecture des signaux de l’encodeur et comparaison
+  // avec les valeurs possibles dans le code de Gray.
+  // Ceci permet de filtrer la plupart des rebonds de l’encodeur.
+  // Les valeurs à 0 représentent les transitions impossibles
+  // et qui doivent être supprimées (resultat = 0).
+  // Les valeurs à 1 représentent les transitions dans le sens horaire.
+  // Les valeurs à -1 représentent les transitions dans le sens anti-horaire.
+  static const int8_t enc_states[ 16 ] PROGMEM =
+    { 0,-1, 1, 0, 1, 0, 0,-1,-1, 0, 0, 1, 0, 1,-1, 0 };
+  static uint8_t old_AB = 0;
+
+  old_AB <<= 2;
+  bitWrite( old_AB, 0, bitRead( ENC_PORT_VAL, ENC_A ) );
+  bitWrite( old_AB, 1, bitRead( ENC_PORT_VAL, ENC_B ) );
+  uint8_t resultat = pgm_read_byte( &enc_states[ ( old_AB & 0x0F ) ] );
+
+  // On accumule “maxVal” résulats dans un sens ou dans l’autre
+  // avant de bouger. Ceci permet de supprimer les derniers rebonds
+  // de l’encodeur qui n’ont pas été filtrés ci-dessus.
+  if( resultat != 0 )
+  {
+    static int8_t resultatCumul = 0;
+    const int8_t maxVal = 2;
+    resultatCumul += resultat;
+    if( resultatCumul >= maxVal )
+    {
+      resultatCumul = maxVal;
+      resultat = 1;
+    }
+    else if( resultatCumul <= -maxVal )
+    {
+      resultatCumul = -maxVal;
+      resultat = -1;
+    }
+    else
+      { resultat = 0; }
+  }
+  return( resultat );
+}
+
+
+void interruptionEncodeur()
+{
+  cli();
+  ENC_PORT_VAL = ENC_PORT;
+  sei();
+  encodeurTourne = true;
+}
